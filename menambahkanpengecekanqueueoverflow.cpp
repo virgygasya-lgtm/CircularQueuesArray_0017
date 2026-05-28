853bbd0ab77088379b8110f8b56319d4f9b755d4
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-28 15:40:42 +0700

    menambahkanpengecekanqueueoverflow.cpp

--- menambahkanpengecekanqueueoverflow.cpp
+++ menambahkanpengecekanqueueoverflow.cpp
@@ -20,3 +20,8 @@ void insert()
     int num;
 cout << "Enter a number: ";
 cin >> num;
+if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+{
+    cout << "\nQueue overflow\n";
+    return;
+}
