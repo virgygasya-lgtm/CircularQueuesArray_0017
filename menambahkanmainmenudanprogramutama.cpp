6d3af538776c1e6c60591c387f69e324989699d4
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-28 15:52:34 +0700

    menambahkanmainmenudanprogramutama.cpp

--- menambahkanmainmenudanprogramutama.cpp
+++ menambahkanmainmenudanprogramutama.cpp
@@ -63,3 +63,16 @@ else
 }
 void display()
 {
+    int main() {
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        cout << "Menu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Implement Delete Operation" << endl;
+        cout << "3. Display values " << endl;
+        cout << "4. Exit " << endl;
+    }
+}
