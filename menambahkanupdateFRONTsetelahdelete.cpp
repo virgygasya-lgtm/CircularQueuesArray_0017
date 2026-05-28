0cb85aac82219ac3d1db839daae1013cf0d1765d
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-28 15:49:48 +0700

    menambahkanupdateFRONTsetelahdelete.cpp

--- menambahkanupdateFRONTsetelahdelete.cpp
+++ menambahkanupdateFRONTsetelahdelete.cpp
@@ -48,3 +48,16 @@ void remove()
 }
 cout << "\nThe element deleted from the queue is: " 
      << queue_array[FRONT] << "\n";
+     if (FRONT == REAR)
+{
+    FRONT = -1;
+    REAR = -1;
+}
+else
+{
+    if (FRONT == max - 1)
+        FRONT = 0;
+    else
+        FRONT = FRONT + 1;
+}
+}
